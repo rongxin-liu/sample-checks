f80e68c3a1241d406f583d22c1e08dc5a87269ce
Rongxin Liu <rongxinliu.dev@gmail.com>
2020-08-12 07:26:27 -0400

    make comments more readable

--- plurality_test.py
+++ plurality_test.py
@@ -11,8 +11,9 @@ def main():
         # vote returns true when given name of first candidate
         # vote returns true when given name of middle candidate
         # vote returns true when given name of last candidate
-        # vote produces correct counts after some have already voted
         assert vote(sys.argv[1], candidates) == True
+
+        # vote produces correct counts after some have already voted
         assert candidates[sys.argv[1]] - before_vote[sys.argv[1]] == 1
         del candidates[sys.argv[1]]
         del before_vote[sys.argv[1]]
@@ -20,8 +21,9 @@ def main():
     else:
 
         # vote returns false when given name of invalid candidate
-        # vote leaves vote counts unchanged when voting for invalid candidate
         assert vote(sys.argv[1], candidates) == False
+
+        # vote leaves vote counts unchanged when voting for invalid candidate
         assert candidates == before_vote
     
 
