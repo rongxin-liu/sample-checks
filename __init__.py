6e49988705fd19ac28d498c1b76a2fc164755b5d
Rongxin Liu <rongxinliu.dev@gmail.com>
2022-04-21 00:32:59 -0400

    updated checks

--- __init__.py
+++ __init__.py
@@ -2,10 +2,11 @@ import check50
 
 @check50.check()
 def install_packages():
-    test_command = "pip3 install cowsay"
-    code = check50.run(test_command).exit()
-    if code != 0:
-        raise check50.Failure(f"expected exit code 0, not {code}")
+    pass
+    # test_command = "pip3 install cowsay"
+    # code = check50.run(test_command).exit()
+    # if code != 0:
+    #     raise check50.Failure(f"expected exit code 0, not {code}")
 
 @check50.check(install_packages)
 def example_test():
